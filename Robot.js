07f6d9dca39bf9d1df1a95f31ded6123746e7987
Thomas Shehan <thomas.shehan88@gmail.com>
2019-10-04 15:20:36 -0400

    Allow creation of additional robots, and have manual task assignment

--- Robot.js
+++ Robot.js
@@ -1,60 +1,36 @@
-const TASKS = [
-  {
-    description: 'do the dishes',
-    eta: 1000,
-  }, {
-    description: 'sweep the house',
-    eta: 3000,
-  }, {
-    description: 'do the laundry',
-    eta: 10000,
-  }, {
-    description: 'take out the recycling',
-    eta: 4000,
-  }, {
-    description: 'make a sammich',
-    eta: 7000,
-  }, {
-    description: 'mow the lawn',
-    eta: 20000,
-  }, {
-    description: 'rake the leaves',
-    eta: 18000,
-  }, {
-    description: 'give the dog a bath',
-    eta: 14500,
-  }, {
-    description: 'bake some cookies',
-    eta: 8000,
-  }, {
-    description: 'wash the car',
-    eta: 20000,
-  },
-];
+
 
 class Robot {
   constructor(name, type) {
     this.name = name;
     this.type = type;
+    this.tasks = [];
+    this.completed = [];
+    this.active = false;
+
     console.log(`New Bot Created: ${name} the ${type}`);
-    this.initializeTasks();
   }
 
-  async initializeTasks() {
-    this.tasks = [...Array(5)].map(() => {
-      const index = Math.floor(Math.random() * TASKS.length);
-      return TASKS[index];
-    });
+  assignTask(newTask) {
+    this.tasks.push(newTask);
+    if (this.active) return;
+    this.activate();
+  }
 
-    for (let task of this.tasks) {
-      task.completed = false;
+  async activate() {
+    this.active = true;
+    while (this.tasks.length > 0) {
+      const task = this.tasks[0];
       console.log(
         `${this.name} has begun: ${task.description}, eta: ${task.eta / 1000} seconds `
       );
       await this.startTask(task);
-      task.completed = true;
+      // Move the first task in this.tasks to the completed tasks
       console.log(`${this.name} has completed: ${task.description}`);
+      this.completed.push(this.tasks.shift());
     }
+    this.active = false;
+    console.log(`${this.name} the ${this.type} has completed all tasks!`);
   }
 
   startTask(task) {
--- index.js
+++ index.js
@@ -7,7 +7,42 @@ const TYPES = {
   AERONAUTICAL: 'Aeronautical'
 };
 
-const robots = {};
+const TASKS = [
+  {
+    description: 'do the dishes',
+    eta: 1000,
+  }, {
+    description: 'sweep the house',
+    eta: 3000,
+  }, {
+    description: 'do the laundry',
+    eta: 10000,
+  }, {
+    description: 'take out the recycling',
+    eta: 4000,
+  }, {
+    description: 'make a sammich',
+    eta: 7000,
+  }, {
+    description: 'mow the lawn',
+    eta: 20000,
+  }, {
+    description: 'rake the leaves',
+    eta: 18000,
+  }, {
+    description: 'give the dog a bath',
+    eta: 14500,
+  }, {
+    description: 'bake some cookies',
+    eta: 8000,
+  }, {
+    description: 'wash the car',
+    eta: 20000,
+  },
+];
+
+const robots = [];
+const names = [];
 
 $(init);
 
@@ -18,7 +53,7 @@ function init() {
 
 function buildTypes() {
   const $dropBox = $('select');
-  for (type of Object.keys(TYPES)) {
+  for (let type of Object.keys(TYPES)) {
     const $option = $('<option>', {
       value: type,
       text: TYPES[type],
@@ -34,15 +69,31 @@ function buildListeners() {
     const type = $('#type').val();
     buildNewBot(name, type);
   });
+
+  $('.js-assign').on('click', assignTasks);
 }
 
 function buildNewBot(name, type) {
   //Check to see if bot of same name already exists
-  if (Object.keys(robots).includes(name)) {
+  if (names.includes(name)) {
     console.error('Robot Already Exists');
     return;
   }
 
   const newBot = new Robot(name, TYPES[type]);
-  robots[name] = newBot;
+  robots.push(newBot);
+  names.push(name);
 }
+
+function assignTasks() {
+  event.preventDefault();
+  for (let robot of robots) {
+    const index = Math.floor(Math.random() * TASKS.length);
+    robot.assignTask(TASKS[index]);
+  }
+}
+
+// this.tasks = [...Array(5)].map(() => {
+//   const index = Math.floor(Math.random() * TASKS.length);
+//   return TASKS[index];
+// });
